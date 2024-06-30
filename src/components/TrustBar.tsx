55525c5423176a500053d21002eff9f7fdd9f0ee
sejfudin <sejfudin.duranovic@gmail.com>
2024-06-30 19:55:03 +0200

    Convert existings JS components into ts|

---
+++ src/components/TrustBar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+interface TrustBarProps {
+    images: string[];
+}
+
+export const TrustBar: React.FC<TrustBarProps> = ({ images }) => {
+    return (
+        <Marquee>
+            {images.map((image) => (
+                <img
+                    width={100}
+                    key={image}
+                    src={image}
+                    className="mx-10"
+                    alt="Logo"
+                />
+            ))}
+        </Marquee>
+    );
+};
